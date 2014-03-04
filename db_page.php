780c6e3bcf740a8c270c8460e974e67f61a58fd5
Patrick Silva <patricksssilva@yahoo.com>
2014-03-04 16:08:20 +0800

    Revision to change in DB

--- db_page.php
+++ db_page.php
@@ -6,7 +6,7 @@ function retrieveRecordsDb(){
 	//$db=new mysqli('records','records','123456','records');
 	//$db=new mysqli('sup-psilva','records','123456','records');
 //	$db=new mysqli('sup-psilva','records_officer','123456','records');
-	$db=new mysqli ( 'mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com','depbeuw9bkz','Gpqq1D7sLGp','depbeuw9bkz');
+	$db=new mysqli ( 'mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com','depbeuw9bkz','Gpqq1jD7sLGp','depbeuw9bkz');
 
 	//$db=new mysqli('192.168.1.128','server_user','123456','records');
 	return $db;
@@ -20,7 +20,7 @@ function retrieveUsersDb(){
 //	$db2=new mysqli('sup-psilva','records','123456','user_management');
 //	$db2=new mysqli('sup-psilva','records_officer','123456','user_management');
 
-	$db2=new mysqli('mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com','depbeuw9bkz','Gpqq1D7sLGp','depbeuw9bkz');
+	$db2=new mysqli('mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com','depbeuw9bkz','Gpqq1jD7sLGp','depbeuw9bkz');
 
 	//$db2=new mysqli('192.168.1.128','server_user','123456','user_management');
 	return $db2;
