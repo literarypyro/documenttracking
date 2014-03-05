f70738e360b0c6276e35b55b38480287ae526daf
Patrick Silva <patricksssilva@yahoo.com>
2014-03-05 15:41:51 +0800

    For the love of God, please connect my database

--- db_page.php
+++ db_page.php
@@ -9,7 +9,7 @@ function retrieveRecordsDb(){
 //	$db=new mysqli ( 'mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com','depbeuw9bkz','Gpqq1jD7sLGp','depbeuw9bkz');
 
 //	$db=new mysqli ( 'localhost','root','','records');
-	$db=$_SESSION['db'];
+	$db=$this->$_SESSION['db'];
 	//$db=new mysqli('192.168.1.128','server_user','123456','records');
 	return $db;
 }
@@ -26,7 +26,7 @@ function retrieveUsersDb(){
 
 //	$db2=new mysqli ( 'localhost','root','','user_management');
 
-	$db2=$_SESSION['db'];
+	$db2=$this->$_SESSION['db'];
 	//$db2=new mysqli('192.168.1.128','server_user','123456','user_management');
 	return $db2;
 }
